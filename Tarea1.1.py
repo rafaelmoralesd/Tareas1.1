4fa0800f89208a776c6c55272c724268c326af99
rafaelmoralesd <149271071+rafaelmoralesd@users.noreply.github.com>
2025-06-06 20:20:44 -0600

    ejercicio 2

--- Tarea1.1.py
+++ Tarea1.1.py
@@ -1,9 +1,10 @@
-#Rafael Ernesto Morales Diaz 
-#20202001873 
-#Tarea  1.1 de Inteligencia Artificial 
+print("Rafael Ernesto Morales Diaz") 
+print("20202001873") 
+print("Tarea  1.1 de Inteligencia Artificial")
+print() 
 
 
-#Ejercicio1
+print("Ejercicio1")
 
 def lista_numeros(lista):
     
@@ -19,3 +20,20 @@ numeros = [3, 9, 4, 1, 14]
 resultado = lista_numeros(numeros)
 
 print("El numero mas de alto de la lista es :", resultado)
+print()
+
+print("---------------------------------------------------")
+
+
+print("Ejercicio 2") 
+
+numero = int(input("Ingrese un numero para la tabla de multiplicar : "))
+
+for i in range(1,11):
+    
+    multiplicacion = numero * i
+    print(f"{numero} x {i} = {multiplicacion}")
+    
+    
+    
+
